fb15a6f497c64750805ab86f562f7fb88ec0f124
RedEpicness <redepicness1@gmail.com>
2014-03-06 18:39:57 +0100

    Added plugin.yml

--- src/net/mcmortals/clans/commands/ClanCommand.java
+++ src/net/mcmortals/clans/commands/ClanCommand.java
@@ -38,31 +38,31 @@ public class ClanCommand extends net.md_5.bungee.api.plugin.Command{
             }
             String arg2 = args[1];
             if(arg2.equals("create")){
-                sender.sendMessage(new ComponentBuilder("Clans help").color(ChatColor.GOLD).append(" - ")
-                        .color(ChatColor.RED).append("create").color(ChatColor.AQUA).create());
+                sender.sendMessage(new ComponentBuilder("----- ").color(ChatColor.AQUA).append("Clans help").color(ChatColor.GOLD).append(" - ")
+                        .color(ChatColor.RED).append("create").color(ChatColor.AQUA).append(" -----").color(ChatColor.AQUA).create());
                 sender.sendMessage(new ComponentBuilder("Usage: ").color(ChatColor.GOLD).append("/guild create <name>")
                         .color(ChatColor.AQUA).create());
             }
             else if(arg2.equals("placeholder1")){
-                sender.sendMessage(new ComponentBuilder("Clans help").color(ChatColor.GOLD).append(" - ")
-                        .color(ChatColor.RED).append("placeholder1").color(ChatColor.AQUA).create());
+                sender.sendMessage(new ComponentBuilder("----- ").color(ChatColor.AQUA).append("Clans help").color(ChatColor.GOLD).append(" - ")
+                        .color(ChatColor.RED).append("placeholder1").color(ChatColor.AQUA).append(" -----").color(ChatColor.AQUA).create());
                 sender.sendMessage(new ComponentBuilder("Usage: ").color(ChatColor.GOLD).append("/guild placeholder1")
                         .color(ChatColor.AQUA).create());
             }
             else if(arg2.equals("placeholder2")){
-                sender.sendMessage(new ComponentBuilder("Clans help").color(ChatColor.GOLD).append(" - ")
-                        .color(ChatColor.RED).append("placeholder2").color(ChatColor.AQUA).create());
+                sender.sendMessage(new ComponentBuilder("----- ").color(ChatColor.AQUA).append("Clans help").color(ChatColor.GOLD).append(" - ")
+                        .color(ChatColor.RED).append("placeholder1").color(ChatColor.AQUA).append(" -----").color(ChatColor.AQUA).create());
                 sender.sendMessage(new ComponentBuilder("Usage: ").color(ChatColor.GOLD).append("/guild placeholder2")
                         .color(ChatColor.AQUA).create());
             }
             else if(arg2.equals("placeholder3")){
-                sender.sendMessage(new ComponentBuilder("Clans help").color(ChatColor.GOLD).append(" - ")
-                        .color(ChatColor.RED).append("placeholder3").color(ChatColor.AQUA).create());
+                sender.sendMessage(new ComponentBuilder("----- ").color(ChatColor.AQUA).append("Clans help").color(ChatColor.GOLD).append(" - ")
+                        .color(ChatColor.RED).append("placeholder1").color(ChatColor.AQUA).append(" -----").color(ChatColor.AQUA).create());
                 sender.sendMessage(new ComponentBuilder("Usage: ").color(ChatColor.GOLD).append("/guild placeholder3")
                         .color(ChatColor.AQUA).create());
             }
             else{
-                sender.sendMessage(new ComponentBuilder("Unknown command: ").color(ChatColor.RED).append(arg2).color(ChatColor.AQUA).create());
+                sender.sendMessage(new ComponentBuilder("Unknown argument: ").color(ChatColor.RED).append(arg2).color(ChatColor.AQUA).create());
             }
 
         }
