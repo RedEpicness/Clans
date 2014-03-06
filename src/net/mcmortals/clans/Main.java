49bac80ddfad7ebfb2ffc31b51c712c23b255ea2
Miha Mitič <RedEpicness@users.noreply.github.com>
2014-03-06 21:46:26 +0100

    Update Main.java

--- src/net/mcmortals/clans/Main.java
+++ src/net/mcmortals/clans/Main.java
@@ -8,6 +8,7 @@ public class Main extends Plugin{
     @Override
     public void onEnable(){
         getProxy().getPluginManager().registerCommand(this, new ClanCommand(this));
+//TODO add all clans from databases into an arrray maybe?? Idk
     }
 
 }
