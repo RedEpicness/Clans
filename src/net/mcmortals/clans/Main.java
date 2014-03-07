b7ffa75353a37c8ad29f3d7a4b3e52fb11c828c4
RedEpicness <redepicness1@gmail.com>
2014-03-07 14:01:26 +0100

    Slowly adding MySQL stuff

--- src/net/mcmortals/clans/Main.java
+++ src/net/mcmortals/clans/Main.java
@@ -1,14 +1,22 @@
 package net.mcmortals.clans;
 
 import net.mcmortals.clans.commands.ClanCommand;
+import net.mcmortals.clans.mysql.MySQL;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import java.sql.Connection;
 
 public class Main extends Plugin{
 
+    MySQL sql;
+    Connection c;
+
     @Override
     public void onEnable(){
+        sql = new MySQL(this, "127.0.0.0", "23456", "database", "user", "pass");
+        c = sql.openConnection();
         getProxy().getPluginManager().registerCommand(this, new ClanCommand(this));
-//TODO add all clans from databases into an arrray maybe?? Idk
+        //TODO add all clans from databases into an array maybe, and create all guild objects
     }
 
 }
